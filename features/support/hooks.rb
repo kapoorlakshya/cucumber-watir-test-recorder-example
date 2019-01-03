cf52e8e08239f579beba354e9bb76db631aa9e68
Lakshya Kapoor <kapoorlakshya@gmail.com>
2019-01-02 23:39:20 -0800

    Update Cucumber Hooks to setup browser and recorder.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,16 +1,32 @@
-Before do
+Before('@watir') do
+
+  #
+  # Setup Browser & Webdrivers gem
+  #
+  @browser_name          = :firefox
+  Webdrivers.install_dir = 'webdrivers_bin'
+
+  #
+  # Setup Recorder
+  #
   opts      = { input:     'desktop',
                 output:    'test-recording.mp4',
                 framerate: 15,
-                log:       'recorder.log',
-                advanced:  { preset: 'ultrafast',
-                             crf:    22 }
+                log:       'recorder.log'
   }
   @recorder = FFMPEG::ScreenRecorder.new(opts)
-  @browser  = Watir::Browser.new @browser_name
+
+  #
+  # Start recording and launch browser
+  #
+  @recorder.start
+  @browser = Watir::Browser.new @browser_name
 end
 
-After do
+#
+# Close browser and stop recording
+#
+After('@watir') do
   @recorder.stop
   @browser.quit
 end
