58be6a99df80a75d711a85702a9508f9c4273157
Lakshya Kapoor <kapoorlakshya@gmail.com>
2021-07-12 00:09:20 -0700

    Use Chrome instead of Firefox

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,7 +3,7 @@ Before do |scenario|
   #
   # Browser browser
   #
-  @browser_name          = :firefox
+  @browser_name          = :chrome
   Webdrivers.install_dir = 'webdrivers_bin'
   recording_name         = scenario.name.gsub(/\s/, '-').downcase + '.mp4'
   @browser               = Watir::Browser.new @browser_name
